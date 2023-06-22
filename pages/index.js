c57018a44a6465e33c6f21f9058983194bff9897
ikram3031 <mdikram.3031@gmail.com>
2023-06-22 21:46:10 +0600

    22 June

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,14 @@ export default function Home() {
           buttonText = "View stores nearby"
           handleOnClick={handleOnBannerBtnClick}
         />
-        <Image 
-          src="/static/hero-image.png" 
-          width={700}
-          height={400}
-        />
+        <div className={styles.heroImage}>
+          <Image 
+            src="/static/hero-image.png" 
+            width={700}
+            height={400}
+          />
+        </div>
+        
       </main>
     </>
   )
