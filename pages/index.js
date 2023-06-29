26f4f4616b5df4abf3a6bae6b49c43d8dad616a5
ikram3031 <mdikram.3031@gmail.com>
2023-06-30 00:16:10 +0600

    30 JUNE

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,19 @@ import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 
 import Banner from '../components/banner';
+import Card from '../components/card';
 
-export default function Home() {
+import coffeeStoresData from "../data/coffee-stores.json";
+
+export async function getStaticProps(context) {
+  return{
+    props:{
+      coffeeStores: coffeeStoresData,
+    }
+  }
+}
+
+export default function Home(props) {
   const handleOnBannerBtnClick = () => {
     // handleTrackLocation();
   };
@@ -28,7 +39,28 @@ export default function Home() {
             height={400}
           />
         </div>
-        
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto Stores</h2>
+          
+            <div className={styles.cardLayout}>
+              {
+                props.coffeeStores.map(coffeeStore => {
+                  return (
+                    <Card
+                      key={coffeeStore.id}
+                      name={coffeeStore.name}
+                      imgUrl={coffeeStore.imgUrl}
+                      href={`/coffee-store/${coffeeStore.id}`}
+                      className={styles.card}
+                    />
+                  )
+                })
+              }
+            </div>
+          </>
+        )}      
+     
       </main>
     </>
   )
